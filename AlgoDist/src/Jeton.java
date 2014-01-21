5be5eb02b582991200649e214d7fbebc394d70f6
cedricfayet <contact@cfayet.eu>
2014-01-21 21:39:44 +0100

    MSJ

---
+++ AlgoDist/src/Jeton.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Queue;
+
+public class Jeton 
+{
+	private Queue<IdentifiantNoeud> file_requete;
+	
+	Jeton()
+	{
+		
+		file_requete = new Queue<IdentifiantNoeud>();
+		
+	}
+	
+}
